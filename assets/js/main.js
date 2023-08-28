85ebd93e60f7811e017455d972e894ddb5d3f7be
francescomascellino <franesco.mascellino@gmail.com>
2023-08-28 16:26:21 +0200

    add bonus request

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,7 @@ Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in
 
 const mailList = ["mail00@mail.com", "mail01@mail.com", "mail02@mail.com", "mail03@mail.com", "mail04@mail.com"]
 
-const userMail = prompt("Inserisci la tua email");
+// const userMail = prompt("Inserisci la tua email");
 
 /* ----------METODO 1 ---------- */
 
@@ -70,11 +70,58 @@ else {
 /* ----------METODO 2 ---------- */
 
 //includes() -> Se mailList include userMail...
-if (mailList.includes(`${userMail}`))
+/* if (mailList.includes(`${userMail}`))
 {
     alert(`"${userMail}" è un indirizzo valido`)
 } 
 
 else {
     alert(`ATTENZIONE: "${userMail}" non è un indirizzo valido`)
+} */
+
+
+/* Bonus
+Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina. */
+
+//Seleziono il div che conterrà il testo del check
+const mailCheckContainer = document.querySelector(".mailCheckContainer");
+
+//Creo l'h1 che diverrà il testo del check
+const mailCheckText = document.createElement("h1");
+
+//inserisco nell'h1 un testo placeholder
+mailCheckText.append("Enter a mail adress to check if it is in the list.");
+
+//inserisco l'h1 nel suo div container
+mailCheckContainer.append(mailCheckText);
+
+//seleziono il bottone usando una variabile
+const mailCheckBtn = document.getElementById("mailCheck");
+
+// aggiungo un listener al click del bottone
+mailCheckBtn.addEventListener("click", function() 
+{
+    //do a userMail il valore del campo input "email"
+    userMail = document.getElementById("userMail").value;
+    console.log(userMail);
+
+    if (mailList.includes(`${userMail}`))
+{
+
+    // //se la mail inserita è in lista viene mostrato un messaggio di esito positivo
+
+    mailCheckText.innerHTML = `The adress "${userMail}" in on the list.`;
+
+} 
+
+else {
+    //se la mail inserita non è in lista viene mostrato un messaggio di esito negativo
+
+    mailCheckText.innerHTML = `WARNING: "${userMail}" in not a valid e-mail adress.`;
+
 }
+})
+
+
+
+
