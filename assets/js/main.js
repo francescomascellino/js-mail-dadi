dad1c83a9de2c60bd66809e4d63396023b1cbe44
francescomascellino <franesco.mascellino@gmail.com>
2023-08-28 14:25:12 +0200

    add dice game

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,23 @@
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve? */
 
+userDice = Math.floor((Math.random() * 6) + 1);
+console.log(`TIRO UTENTE = ${userDice}`);
+
+computerDice = Math.floor((Math.random() * 6) + 1);
+console.log(`TIRO DELLA MACCHINA = ${computerDice}`);
+
+if (userDice > computerDice) {
+    console.log("User wins");
+} 
+
+else if (userDice < computerDice) {
+    console.log("Machine wins");
+} 
+
+else {
+    console.log("Tie");
+}
 
 /* Mail
 Chiedi all'utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
